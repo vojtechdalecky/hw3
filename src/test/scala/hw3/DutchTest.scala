431460cd487130d3f3464e79c766e6f532fccc79
Konrad Siek <konrad.siek@gmail.com>
2020-01-07 14:57:35 +0100

    Ooops.

--- src/test/scala/hw3/DutchTest.scala
+++ src/test/scala/hw3/DutchTest.scala
@@ -65,38 +65,37 @@ class DutchTest extends FunSuite with Matchers {
       (Competitor("Henry Bretislaus", Some(3)), Some(Competitor("Ottokar II", Some(1)))),
       (Competitor("Wenceslaus I", Some(2)), Some(Competitor("Ottokar I ", Some(0))))
     )
+  }
 
+  test("Odd competitors") {
 
-    test("Odd competitors") {
+    val pairing = dutch(competitors = competitorsOdd, groupSize = 4)
 
-      val pairing = dutch(competitors = competitorsOdd, groupSize = 4)
-
-      /*
-       * group I:
-       *   Wenceslaus II      10        Pair 1
-       *   Spytihněv          9         Pair 2
-       *   Svatopluk          8         Pair 1
-       *   Wenceslaus III     7         Pair 2
-       *
-       * group II:
-       *   Bořivoj I          6         Pair 3
-       *   Vladislaus Henry   5         Pair 4
-       *   Sobeslaus I        4         Pair 3
-       *   Henry Bretislaus   3         Pair 4
-       *
-       * group III:
-       *   Wenceslaus I       2         Pair 5
-       *   Ottokar II         1         Not paired
-       *   Ottokar I          0         Pair 5
-       */
-      pairing shouldBe Set(
-        (Competitor("Wenceslaus II", Some(10)), Some(Competitor("Svatopluk", Some(8))),
-        (Competitor("Spytihněv I", Some(9)), Some(Competitor("Wenceslaus III", Some(7)))),
-        (Competitor("Bořivoj I", Some(6))), Some(Competitor("Sobeslaus I", Some(4)))),
-        (Competitor("Vladislaus Henry", Some(5)), Some(Competitor("Henry Bretislaus", Some(3)))),
-        (Competitor("Wenceslaus I", Some(2)), Some(Competitor("Ottokar I ", Some(0)))),
-        (Competitor("Ottokar II", Some(1)), None)
-      )
-    }
+    /*
+     * group I:
+     *   Wenceslaus II      10        Pair 1
+     *   Spytihněv          9         Pair 2
+     *   Svatopluk          8         Pair 1
+     *   Wenceslaus III     7         Pair 2
+     *
+     * group II:
+     *   Bořivoj I          6         Pair 3
+     *   Vladislaus Henry   5         Pair 4
+     *   Sobeslaus I        4         Pair 3
+     *   Henry Bretislaus   3         Pair 4
+     *
+     * group III:
+     *   Wenceslaus I       2         Pair 5
+     *   Ottokar II         1         Not paired
+     *   Ottokar I          0         Pair 5
+     */
+    pairing shouldBe Set(
+      (Competitor("Wenceslaus II", Some(10)), Some(Competitor("Svatopluk", Some(8))),
+      (Competitor("Spytihněv I", Some(9)), Some(Competitor("Wenceslaus III", Some(7)))),
+      (Competitor("Bořivoj I", Some(6))), Some(Competitor("Sobeslaus I", Some(4)))),
+      (Competitor("Vladislaus Henry", Some(5)), Some(Competitor("Henry Bretislaus", Some(3)))),
+      (Competitor("Wenceslaus I", Some(2)), Some(Competitor("Ottokar I ", Some(0)))),
+      (Competitor("Ottokar II", Some(1)), None)
+    )
   }
 }
